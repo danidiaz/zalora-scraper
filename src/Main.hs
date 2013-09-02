050ea257e184dd21401343a9bf624eed57f776c6
Daniel Diaz <diaz.carrete@facebook.com>
2013-09-02 23:34:24 +0200

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE CPP             #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Main where
+
+------------------------------------------------------------------------------
+import           Control.Exception (SomeException, try)
+import           System.IO
+
+main :: IO ()
+main = do
+    return ()
