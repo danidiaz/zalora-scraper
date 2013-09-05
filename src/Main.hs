834676b4e261157e4200a759dfa28de1812fe1c2
Daniel Diaz <diaz.carrete@facebook.com>
2013-09-05 22:55:51 +0200

    applicative parser

--- src/Main.hs
+++ src/Main.hs
@@ -9,11 +9,14 @@ import           Control.Exception (SomeException, try)
 import           Control.Applicative
 import           Control.Monad hiding (mapM_)
 import           Control.Monad.Trans
-import           Control.Monad.Logic 
-import           Data.Foldable ()
+import           Control.Monad.Logic (observeAll)
+import           Data.Foldable (mapM_)
 import           Data.Traversable
+import           Data.Char
+import           Data.Maybe
+import           Data.Monoid
 import           System.IO
-import           Prelude
+import           Prelude hiding (takeWhile,mapM_)
 
 import           System.IO.Streams (InputStream, OutputStream, stdout)
 import qualified System.IO.Streams as Streams
@@ -21,6 +24,9 @@ import           Data.Text (Text)
 import qualified Data.Text as T
 import           Data.Text.Encoding
 import           Data.Text.IO as TIO
+
+import           Data.Attoparsec.Text
+import           Data.Attoparsec.Combinator
 
 import           Text.HTML.TagSoup
 
@@ -31,20 +37,24 @@ replusify = msum . map return
 matches :: Tag Text -> Tag Text -> Bool
 matches pattern tag = tag ~== pattern
 
-scripts :: [Tag Text] -> [Text]
-scripts tags = observeAll $ do
-    (_:TagText txt:_) <- replusify $ partitions (matches $ TagOpen "script"  []) tags
-    let mark = "wt.contentGroup = {" 
-    (_:txt':_) <- return $ T.splitOn mark txt
-    -- guard $ T.isInfixOf mark txt
-    return txt'
+parseCG :: Parser [Text] 
+parseCG = 
+    manyTill' anyChar (string "wt.contentGroup = {") *> sepBy' entry (char ',')
+    where
+        ss = skipSpace
+        entry =  ss *> skipMany digit *> ss *> char ':' *> ss 
+                        *> char '"' *> takeWhile isAlpha <* char '"'
+
+keywordList :: [Tag Text] -> Maybe [Text]
+keywordList tags = listToMaybe . observeAll $ do
+    (_:TagText txt:_) <- replusify $ partitions (matches $ TagOpen "script" []) tags
+    replusify . maybeToList . maybeResult $ parse parseCG txt
 
 main :: IO ()
 main = do
     soup <- parseTags . decodeUtf8 <$> get "http://www.zalora.sg/" concatHandler'
-    -- mapM_ (putStrLn . show) soup
-    let s = scripts soup 
-    mapM_ TIO.putStrLn s
+    mapM_ (mapM_ TIO.putStrLn) (keywordList soup)
+    return ()
 
 
 
