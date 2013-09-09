9ce7a2b6c3bebb9024208e70b920dc79cab9ce7e
Daniel Diaz <diaz.carrete@facebook.com>
2013-09-09 16:20:59 +0200

    pipes!

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,8 @@ import           Data.Traversable
 import           Data.Char
 import           Data.Maybe
 import           Data.Monoid
+import qualified Data.Set as S
+import qualified Data.Map as M
 import           Data.List
 import           System.IO
 import           Prelude hiding (mapM_)
@@ -26,6 +28,9 @@ import qualified Data.Text as T
 import           Data.Text.Encoding
 import           Data.Text.IO as TIO
 
+import           Pipes
+import           Pipes.Core
+
 import           Data.Attoparsec.Text hiding (takeWhile)
 import qualified Data.Attoparsec.Text as A
 import           Data.Attoparsec.Combinator
@@ -36,13 +41,14 @@ import           Network.Http.Client
 
 matches :: Tag Text -> Tag Text -> Bool
 matches pattern tag = tag ~== pattern
+
 -- wt.contentId = "shop.pc.brand.mexx"
 parseKeywords :: Parser [Text] 
 parseKeywords = 
        manyTill' (takeTill isEndOfLine *> endOfLine)
                  (string "wt.contentId" *> _char '=' *> _char '"') 
     *> sepBy' (takeTill $ \c -> c == '.' || c == '"') (char '.')
-    where _char x = skipSpace *> char x
+    where _char x = skipSpace *> char x 
 
 scrapeKeywords :: [Tag Text] -> Maybe [Text]
 scrapeKeywords tags = listToMaybe $ do
@@ -51,14 +57,25 @@ scrapeKeywords tags = listToMaybe $ do
         takeWhile (not.T.null) <$> parse parseKeywords txt
 
 scrapeLinks :: [Tag Text] -> [Text]
-scrapeLinks = 
-   map (fromAttrib "href") . filter (matches $ TagOpen "a" [("href","")])
+scrapeLinks =  filter (not . T.isSuffixOf ".html")
+             . filter (not . T.isPrefixOf "http")
+             . map (fromAttrib "href") 
+             . filter (matches $ TagOpen "a" [("href","")])
+
+scrapeSKUs :: [Tag Text] -> [Text]
+scrapeSKUs =  concatMap (maybeToList . T.stripPrefix ":" . snd . T.breakOn ":")
+            . map (fromAttrib "id") 
+            . filter (matches $ TagOpen "a" [("href",""),("class","itm-link"),("id","")])
+
+pageServer :: Monad m => Text -> Server (S.Set Text) (M.Map Text [Tag Text]) m a 
+pageServer baseURL = forever $ undefined
 
 main :: IO ()
 main = do
-    soup <- parseTags . decodeUtf8 <$> get "http://www.zalora.sg/" concatHandler'
+    soup <- parseTags . decodeUtf8 <$> get "http://www.zalora.sg/womens/clothing/shorts" concatHandler'
     mapM_ (mapM_ TIO.putStrLn) (scrapeKeywords soup)
-    -- mapM_ TIO.putStrLn (scrapeLinks soup)
+    mapM_ TIO.putStrLn (scrapeLinks soup)
+    mapM_ TIO.putStrLn (scrapeSKUs soup)
     return ()
 
 
